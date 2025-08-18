0d83826f58fca24a975eaa4e8f8a56faf847aa97
hidecard <hidecard1500@gmail.com>
2025-08-18 16:38:10 +0630

    Lesson 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.orange,
-            title: Text("YHA Flutter Lesson"),
-          ),
-          body: Center(
-            child: Text(
-              "Hello YHA",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( // Root widget
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My Home Page'),
+        ),
+        body: Center(
+          child: Builder(
+            builder: (context) {
+              return Column(
+                children: [
+                  const Text('Hello, World!'),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('Click!');
+                    },
+                    child: const Text('A button'),
+                  ),
+                ],
+              );
+            },
           ),
         ),
       ),
     );
+  }
+}
