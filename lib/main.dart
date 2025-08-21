80a8f7ad648a67543c784124a6ddb89fbd238f62
hidecard <hidecard1500@gmail.com>
2025-08-21 16:08:13 +0630

    Flex lesson

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('YHA Lesson')),
-        body: Container(
-          width: double.infinity,
-          height: 200,
-          color: Colors.grey[300],
-          child: Align(
-            alignment: Alignment.topLeft,
-            child: Container(
-              width: 100,
-              height: 100,
-              color: Colors.orange,
-              child: const Center(
-                child: Text('Aligned Box', style: TextStyle(color: Colors.white)),
+        body: Row(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Container(
+                height: 100,
+                color: Colors.blue,
+                child: const Center(child: Text('Flex 1', style: TextStyle(color: Colors.white))),
               ),
             ),
-          ),
+            Flexible(
+              flex: 3,
+              child: Container(
+                height: 100,
+                color: Colors.green,
+                child: const Center(child: Text('Flex 3', style: TextStyle(color: Colors.white))),
+              ),
+            ),
+          ],
         ),
       ),
     );
