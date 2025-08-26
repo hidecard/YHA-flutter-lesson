f6b47bfd6e0a72e6f4f185c60d001add96c8969c
hide card <hidecard1500@gmail.com>
2025-08-26 11:27:51 +0630

    gird

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('ListView.separated Example')),
-        body: ListView.separated(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('Item $index'),
-              leading: Icon(Icons.check_circle),
-            );
-          },
-          separatorBuilder: (context, index) {
-            return Divider(color: Colors.grey);
-          },
+        appBar: AppBar(title: Text('GridView.count Example')),
+        body: GridView.count(
+          crossAxisCount: 2,
+          mainAxisSpacing: 8.0,
+          crossAxisSpacing: 8.0,
+          padding: EdgeInsets.all(8.0),
+          children: [
+            Container(color: Colors.blue, child: Center(child: Text('Item 1'))),
+            Container(color: Colors.red, child: Center(child: Text('Item 2'))),
+            Container(color: Colors.green, child: Center(child: Text('Item 3'))),
+            Container(color: Colors.yellow, child: Center(child: Text('Item 4'))),
+          ],
         ),
       ),
     );
